c87ffa6f2cd300bf392a758ba37add3b5072366d
moriarty <wang_jn_xian@163.com>
2022-06-26 13:24:40 +0800

    change template and react-scripts

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -2,13 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mode = process.evn.NODE_ENV;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>You are currently at {mode} mode.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
